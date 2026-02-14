83bf261ac384d6680d5fb3c8c6a471ad14b1c446
danhergir <18383990+danhergir@users.noreply.github.com>
2026-02-14 19:44:31 +0000

    seshions 0.1.6

--- Formula/seshions.rb
+++ Formula/seshions.rb
@@ -1,17 +1,17 @@
 class Seshions < Formula
   desc "Monitor Codex/Claude terminal sessions with macOS notifications"
   homepage "https://github.com/danhergir/seshions"
-  url "https://github.com/danhergir/seshions/releases/download/v0.1.5/seshions-0.1.5.tar.gz"
-  sha256 "21eaaf03ce90c51da1c5abef0d994f8b29e778798dcf036e56827f6ab4af357e"
+  url "https://github.com/danhergir/seshions/releases/download/v0.1.6/seshions-0.1.6.tar.gz"
+  sha256 "0022fbb7ff09727ebad47dca9cd72475cc995c31eee9dec929c88ef6fd3fe89d"
   license "MIT"
 
   depends_on "node"
 
   def install
-    libexec.install Dir["src/*.js"]
+    libexec.install Dir["src"]
     (bin/"seshions").write <<~EOS
       #!/bin/bash
-      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/index.js" "$@"
+      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/src/index.js" "$@"
     EOS
     chmod 0755, bin/"seshions"
   end
