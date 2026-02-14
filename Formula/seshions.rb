e1d48629f41da10d6dd126ca50c4112dad293899
danhergir <18383990+danhergir@users.noreply.github.com>
2026-02-14 18:28:16 +0000

    seshions 0.1.4

--- Formula/seshions.rb
+++ Formula/seshions.rb
@@ -1,8 +1,8 @@
 class Seshions < Formula
   desc "Monitor Codex/Claude terminal sessions with macOS notifications"
   homepage "https://github.com/danhergir/seshions"
-  url "https://github.com/danhergir/seshions/releases/download/v0.1.3/seshions-0.1.3.tar.gz"
-  sha256 "aca643bd3576f4e87ca38fa760411d9caa031faa0097f44330f8f589f59ac23f"
+  url "https://github.com/danhergir/seshions/releases/download/v0.1.4/seshions-0.1.4.tar.gz"
+  sha256 "5cf4bb5000a205e7378176522f201430085a7868d04a715063c58ee49a437fca"
   license "MIT"
 
   depends_on "node"
